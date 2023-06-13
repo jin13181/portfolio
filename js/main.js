3c102d33d1e1c924b98d7b9c5c579783943119fb
jin13181 <jin13181@gmail.com>
2023-06-13 23:22:45 +0900

    토글메뉴수정, 스크롤에 반응하는 배경 추가

--- js/main.js
+++ js/main.js
@@ -83,10 +83,6 @@ const navTogglerBtn = document.querySelector(".nav-toggler"),
   aside = document.querySelector(".aside");
 
 navTogglerBtn.addEventListener("click", () => {
-  if (navTogglerBtn.classList.contains("open")) {
-    navTogglerBtn.classList.remove("open");
-    console.log("...");
-  }
   asideSectionTogglerBtn();
 });
 
@@ -96,57 +92,6 @@ function asideSectionTogglerBtn() {
   for (let i = 0; i < totalSection; i++) {
     allSection[i].classList.toggle("open");
   }
-}
-
-/* ===== MIXITUP FILTER PORTFOLIO ===== */
-
-// let mixer = mixitup(".portfolio__container", {
-//   selectors: {
-//     target: ".portfolio__card",
-//   },
-//   animation: {
-//     duration: 300,
-//   },
-// });
-
-/* ===== Link Active Portfolio ===== */
-
-// Portfolio
-const $$portfolioItem = document.querySelectorAll(".portfolio__item");
-
-function activePortfolio() {
-  $$portfolioItem.forEach((l) => l.classList.remove("active-portfolio"));
-  this.classList.add("active-portfolio");
-}
-
-$$portfolioItem.forEach((l) => l.addEventListener("click", activePortfolio));
-
-/* ===== Portfolio Popup ===== */
-document.addEventListener("click", (e) => {
-  if (e.target.classList.contains("portfolio__button")) {
-    togglePortfolioPopup();
-    portfolioItemDetails(e.target.parentElement);
-  }
-});
-
-function togglePortfolioPopup() {
-  document.querySelector(".portfolio__popup").classList.toggle("open");
-}
-
-document
-  .querySelector(".portfolio__popup-close")
-  .addEventListener("click", togglePortfolioPopup);
-
-function portfolioItemDetails(portfolioItem) {
-  // console.log(portfolioItem)
-  document.querySelector(".pp__thumbnail img").src =
-    portfolioItem.querySelector(".portfolio__img").src;
-  document.querySelector(".portfolio__popup-subtitle span").innerHTML =
-    portfolioItem.querySelector(".portfolio__subtitle").innerHTML;
-  document.querySelector(".portfolio__popup-title").innerHTML =
-    portfolioItem.querySelector(".portfolio__title").innerHTML;
-  document.querySelector(".portfolio__popup-body").innerHTML =
-    portfolioItem.querySelector(".portfolio__item-details").innerHTML;
 }
 
 // Email JS
--- js/scroll.js
+++ js/scroll.js
@@ -9,30 +9,124 @@ function vh(v) {
 $(function () {
   $(window).scroll(function () {
     if ($(this).scrollTop() >= 0 && $(this).scrollTop() <= vh(50)) {
-      $(".about").css("background", "#cdc4b5").css("transition", "0.5s");
-      $(".about p, .about h2, .about h3, .about h4, .about h2::before")
-        .css("color", "#203929")
-        .css("transition", "0.5s");
-      $(".section-title h2:before")
-        .css("backgroundColor", "#203929")
-        .css("transition", "0.5s");
+      //main 배경 베이지
+      $(".main").css(
+        "background",
+        '#efebe6 url("https://portfolio-v4-abjt14.vercel.app/assets/images/noise/noise.png")'
+      );
+      $(".main h3, .main a").css("color", "#203929");
+      $(".topNav .nav li a").css("color", "#203929");
+
+      //about 배경 초록
+      $(".about").css(
+        "background",
+        '#203929 url("https://portfolio-v4-abjt14.vercel.app/assets/images/noise/noise.png")'
+      );
+      $(".about p, .about h2, .about h3, .about h4, .about span, .about a").css(
+        "color",
+        "#cdc4b5"
+      );
+
+      //제이쿼리 가상 선택자 선택안됨
+      $(".section-title h2:before").css("background-color", "#efebe6");
+      $(".about .timeline-item::before").css("background-color", "#efebe6");
     } else if (
       $(this).scrollTop() >= vh(50) &&
+      $(this).scrollTop() <= vh(100)
+    ) {
+      $(".topNav .nav li a").css("color", "#203929");
+    } else if (
+      $(this).scrollTop() >= vh(50) &&
+      $(this).scrollTop() <= vh(150)
+    ) {
+      //about 배경 베이지
+      $(".about")
+        .css(
+          "background",
+          '#efebe6 url("https://portfolio-v4-abjt14.vercel.app/assets/images/noise/noise.png")'
+        )
+        .css("transition", "0.5s");
+      $(".about p, .about h2, .about h3, .about h4, .about span, .about a").css(
+        "color",
+        "#203929"
+      );
+      $(".about .btn")
+        .css("color", "#efebe6")
+        .css("background-color", "#203929");
+      $(".topNav .nav li a").css("color", "#203929");
+    } else if (
+      $(this).scrollTop() >= vh(150) &&
+      $(this).scrollTop() <= vh(200)
+    ) {
+      //about 배경 초록
+      $(".about").css(
+        "background",
+        '#203929 url("https://portfolio-v4-abjt14.vercel.app/assets/images/noise/noise.png")'
+      );
+      $(".about p, .about h2, .about h3, .about h4, .about span, .about a").css(
+        "color",
+        "#cdc4b5"
+      );
+      $(".topNav .nav li a").css("color", "#cdc4b5");
+      $(".about .btn").css("color", "#203929");
+      $(".about .btn").css("background-color", "#cdc4b5");
+
+      //제이쿼리 가상 선택자 선택안됨
+      $(".section-title h2:before").css("background-color", "#efebe6");
+      $(".about .timeline-item::before").css("background-color", "#efebe6");
+
+      //stack 배경 초록
+      $(".stack")
+        .css(
+          "background",
+          '#203929 url("https://portfolio-v4-abjt14.vercel.app/assets/images/noise/noise.png")'
+        )
+        .css("transition", "0.5s");
+      $(".stack p, .stack h2, .stack h3, .stack h4, .stack h2::before").css(
+        "color",
+        "#cdc4b5"
+      );
+      $(".stackBox").css("border-bottom", "1px solid #efebe6");
+      $(".topNav .nav li a").css("color", "#cdc4b5");
+    } else if (
+      $(this).scrollTop() >= vh(200) &&
       $(this).scrollTop() <= vh(300)
     ) {
-      $(".about").css("background", "#203929").css("transition", "0.5s");
-      $("p, h2, h3, h4, h2::before")
-        .css("color", "#203929")
+      //about 배경 베이지
+      $(".about")
+        .css(
+          "background",
+          '#efebe6 url("https://portfolio-v4-abjt14.vercel.app/assets/images/noise/noise.png")'
+        )
         .css("transition", "0.5s");
+      $(".about p, .about h2, .about h3, .about h4, .about span, .about a").css(
+        "color",
+        "#203929"
+      );
+      $(".about .btn")
+        .css("color", "#efebe6")
+        .css("background-color", "#203929");
+      $(".topNav .nav li a").css("color", "#203929");
+      //stack 배경 베이지
+      $(".stack")
+        .css(
+          "background",
+          '#efebe6 url("https://portfolio-v4-abjt14.vercel.app/assets/images/noise/noise.png")'
+        )
+        .css("transition", "0.5s");
+      $(".stack p, .stack h2, .stack h3, .stack h4, h2::before").css(
+        "color",
+        "#203929"
+      );
+      $(".stackBox").css("border-bottom", "1px solid #203929");
+      $(".topNav .nav li a").css("color", "#203929");
     } else if (
-      $(this).scrollTop() >= vh(300) &&
-      $(this).scrollTop() <= vh(350)
+      $(this).scrollTop() >= vh(370) &&
+      $(this).scrollTop() <= vh(480)
     ) {
-      $(".section").css("background", "#203929").css("transition", "0.5s");
-    } else if ($(this).scrollTop() >= 1400 && $(this).scrollTop() <= 1900) {
-      $(".section").css("background", "#203929").css("transition", "0.5s");
-    } else if ($(this).scrollTop() >= 2000 && $(this).scrollTop() <= 2500) {
-      $(".section").css("background", "#cdc4b5").css("transition", "0.5s");
+      $(".topNav .nav li a").css("color", "#efebe6");
+    } else if ($(this).scrollTop() <= vh(1000)) {
+      $(".topNav .nav li a").css("color", "#203929");
     }
   });
 });
